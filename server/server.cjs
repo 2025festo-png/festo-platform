28cbdcf86e3ced5358b74853c2d152b747de4d46
2025festo-png <2025festo@gmail.com>
2026-08-10 23:27:27 +0200

    Update server.cjs

--- server/server.cjs
+++ server/server.cjs
@@ -21,11 +21,10 @@ const PORT = process.env.PORT || 3000;
 //    CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET
 // ============================================================
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'ej5pmsyk',
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
-
 // ============================================================
 // KONFIGURIMI MULTER
 // ============================================================
